483b988d8de34405a71dbc6c3b697cce228aab16
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-02-06 15:41:10 +0100

    :art: Add unix-style line-ending in linter

--- src/scenes/App/index.js
+++ src/scenes/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import './style.css'
 import theme from '~/toolbox/theme'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
 import '~/toolbox/theme.css'
