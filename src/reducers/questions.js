bd7879ed587eb11b5e62e6d1bbd73767a1382d81
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-02-08 14:36:09 +0100

    :zap::lipstick: improve search

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -11,20 +11,24 @@ const initialState = {
 const fuseOptions = {
   shouldSort: true,
   includeScore: true,
+  tokenize: true,
   threshold: 0.6,
   location: 0,
-  distance: 100,
+  distance: 10000,
   maxPatternLength: 32,
-  minMatchCharLength: 1,
+  minMatchCharLength: 3,
   keys: ['question', 'answer']
 }
 
 const search = (state = initialState, action) => {
   switch (action.type) {
   case types.SEARCH:
+    const filtered = new Fuse(state.questions, fuseOptions)
+      .search(action.text)
+      .filter(q => q.score < fuseOptions.threshold)
     return {
       ...state,
-      filtered: new Fuse(state.questions, fuseOptions).search(action.text)
+      filtered: filtered
     }
   case types.CLEAR_SEARCH:
     return {
--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -5,25 +5,50 @@ import { connect } from 'react-redux'
 import Searchbar from 'components/Searchbar'
 import QuestionCard from 'components/QuestionCard'
 
+import './style.css'
+
 class Home extends Component {
   render () {
-    const { questions, filtered } = this.props
+    const { questions, filtered, searchText } = this.props
 
-    const list = filtered.length > 0 ? filtered.map(x => x.item) : questions
+    const list =
+      filtered.length > 0
+        ? filtered.map(x => x.item)
+        : questions.reverse().slice(0, 10)
 
     const QuestionsList = list.map((question, index) => (
-      <QuestionCard
-        style={{ width: '70%', marginLeft: 'auto', marginRight: 'auto' }}
-        question={question}
-        key={index}
-      />
+      <QuestionCard question={question} key={index} />
     ))
+
+    let Results
+
+    if (searchText === '') {
+      Results = (
+        <div>
+          <p className="indication">Latest questions</p>
+          {QuestionsList}
+        </div>
+      )
+    } else if (filtered.length > 0) {
+      Results = (
+        <div>
+          <p className="indication">{filtered.length} results found</p>
+          {QuestionsList}
+        </div>
+      )
+    } else {
+      Results = (
+        <p className="indication" style={{ textAlign: 'center' }}>
+          Nothing found &nbsp;<i className="material-icons">sms_failed</i>
+        </p>
+      )
+    }
 
     return (
       <div className="Home">
         <h1 style={{ textAlign: 'center' }}>FAQ Zenika</h1>
         <Searchbar style={{ marginTop: '3rem', marginBottom: '2rem' }} />
-        {QuestionsList}
+        <div className="results">{Results}</div>
       </div>
     )
   }
@@ -31,9 +56,14 @@ class Home extends Component {
 
 Home.propTypes = {
   questions: PropTypes.array.isRequired,
-  filtered: PropTypes.array.isRequired
+  filtered: PropTypes.array.isRequired,
+  searchText: PropTypes.string.isRequired
 }
 
-const mapStateToProps = state => state.questions
+const mapStateToProps = state => ({
+  questions: state.questions.questions,
+  filtered: state.questions.filtered,
+  searchText: state.search.text
+})
 
 export default connect(mapStateToProps)(Home)
