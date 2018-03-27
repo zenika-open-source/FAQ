21fd4347c53e72753fb9bbce74e4d31c9b47ee82
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-03-27 12:12:29 +0200

    Use Zenika colors for brands

--- src/scenes/App/components/Footer/Footer.jsx
+++ src/scenes/App/components/Footer/Footer.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 const Footer = () => (
   <div className="footer">
-    <a href="https://graph.coom" target="_blank" style={{ display: 'flex' }}>
+    <a
+      href="https://graph.coom"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ display: 'flex' }}
+    >
       <img
         src="/img/brands/backend-by-graphcool.svg"
         alt="Backend by Graphcool"
         style={{ height: '30px' }}
       />
     </a>
-    <a href="https://www.algolia.com" target="_blank">
+    <a href="https://www.algolia.com" target="_blank" rel="noopener noreferrer">
       <img src="/img/brands/powered-by-algolia.svg" alt="Powered by Algolia" />
     </a>
   </div>
