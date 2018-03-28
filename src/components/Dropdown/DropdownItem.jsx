140058a74dd18966c8efb3eb3771865878df4507
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-03-28 14:55:59 +0200

    :bug: Fix dropdown link not triggering (#31)

--- src/components/Dropdown/DropdownItem.jsx
+++ src/components/Dropdown/DropdownItem.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const DropdownItem = ({ children, icon, disabled, onClick, ...otherProps }) => (
+const DropdownItem = ({
+  children,
+  icon,
+  disabled,
+  onClick,
+  href,
+  target,
+  ...otherProps
+}) => (
   <a
     tabIndex={disabled ? null : 0}
     onMouseDown={e => {
       if (disabled) return
       if (onClick) return onClick()
+      if (href) return window.open(href, target === '_blank' ? target : '_self')
     }}
     disabled={disabled}
     {...otherProps}
@@ -25,7 +34,9 @@ DropdownItem.propTypes = {
   children: PropTypes.node.isRequired,
   icon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   disabled: PropTypes.bool,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  href: PropTypes.string,
+  target: PropTypes.string
 }
 
 export default DropdownItem
