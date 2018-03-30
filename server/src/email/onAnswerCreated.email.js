35b751a01c2376f736619a25f0dbf37dc42cb894
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-03-30 15:18:41 +0200

    :bug: Fix email header

--- server/src/email/onAnswerCreated.email.js
+++ server/src/email/onAnswerCreated.email.js
@@ -245,7 +245,7 @@ a[x-apple-data-detectors=true] {
 
                     <div align="center" class="img-container center  autowidth  fullwidth " style="padding-right: 0px;  padding-left: 0px;">
 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px;line-height:0px;"><td style="padding-right: 0px; padding-left: 0px;" align="center"><![endif]-->
-  <img class="center  autowidth  fullwidth" align="center" border="0" src="images/image.png" alt="Image" title="Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: 0;height: auto;float: none;width: 100%;max-width: 700px" width="700">
+  <img class="center  autowidth  fullwidth" align="center" border="0" src="https://faq.zenika.com/img/email_header.png" alt="FAQ Zenika - Image" title="FAQ Zenika - Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: 0;height: auto;float: none;width: 100%;max-width: 700px" width="700">
 <!--[if mso]></td></tr></table><![endif]-->
 </div>
 
