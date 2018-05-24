3b59f1ba33eb872af0a8a5cde79013cdc35cd45e
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-05-24 11:10:31 +0200

    :bug: Fix history not updated

--- src/services/history.js
+++ src/services/history.js
@@ -23,15 +23,40 @@ const addActionQuery = gql`
   }
 `
 
+export const loadHistoryQuery = gql`
+  query($nodeId: ID!) {
+    allHistoryActions(filter: { node: { id: $nodeId } }) {
+      id
+      node {
+        id
+      }
+      action
+      model
+      meta
+      user {
+        id
+        name
+        picture
+      }
+      createdAt
+    }
+  }
+`
+
 const history = {
   addAction: (action, model, meta, nodeId) => {
-    return apollo.mutate({
-      mutation: addActionQuery,
-      variables: { action, model, meta, nodeId, userId: auth.getUserNodeId() }
-    })
+    return apollo
+      .mutate({
+        mutation: addActionQuery,
+        variables: { action, model, meta, nodeId, userId: auth.getUserNodeId() }
+      })
+      .then(() =>
+        apollo.query({
+          query: loadHistoryQuery,
+          variables: { nodeId }
+        })
+      )
   }
 }
-
-window.serviceHistory = history
 
 export default history
