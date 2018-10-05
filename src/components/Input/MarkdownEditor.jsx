dcd411b1ea3493cf0ae74a81d6324d2e6f7f9f7b
Errorname <do.not.press.enter@gmail.com>
2018-10-05 12:17:59 +0200

    :bug: Fix focus on source change

--- src/components/Input/MarkdownEditor.jsx
+++ src/components/Input/MarkdownEditor.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
 import ReactMde, { ReactMdeCommands } from 'react-mde'
@@ -7,15 +7,21 @@ import 'react-mde/lib/styles/css/react-mde-all.css'
 
 import './Input.css'
 
-const MarkdownEditor = ({ content, onChange }) => (
-  <ReactMde
-    className="markdown-editor"
-    value={content}
-    showdownFlavor="github"
-    onChange={onChange}
-    commands={ReactMdeCommands.getDefaultCommands()}
-  />
-)
+class MarkdownEditor extends PureComponent {
+  render() {
+    const { content, onChange } = this.props
+
+    return (
+      <ReactMde
+        className="markdown-editor"
+        value={content}
+        showdownFlavor="github"
+        onChange={onChange}
+        commands={ReactMdeCommands.getDefaultCommands()}
+      />
+    )
+  }
+}
 
 MarkdownEditor.propTypes = {
   content: PropTypes.object,
