70e2f18153723810cecdb56699793af51c0cd609
Gabriel518 <charrongabriel51@gmail.com>
2019-02-06 16:37:58 +0100

    Revert ":ok_hand: Some improvements"
    This reverts commit dbcd36a0aa9af70b62836546d897aca555eb4609.

--- src/scenes/App/components/Navbar/Navbar.jsx
+++ src/scenes/App/components/Navbar/Navbar.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import { auth, configuration } from 'services'
-
-import { Button } from 'components'
+import Button from 'components/Button'
 
 import GithubIcon from './components/GithubIcon'
 import UserMenu from './components/UserMenu'
@@ -19,19 +18,19 @@ const Navbar = () => (
       </div>
     </Link>
     <div className="navigation">
-      <a
-        className="report-bug"
-        href="https://github.com/Zenika/FAQ"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://github.com/Zenika/FAQ" target="_blank" rel="noopener noreferrer">
         <GithubIcon />
         <span>report a bug</span>
       </a>
       {auth.isAuthenticated() && <UserMenu />}
-      <Link to="/q/new" className="new-question">
-        <Button icon="record_voice_over" label="New question" primary fixed />
-      </Link>
+      <Link to="/q/new">
+          <Button
+            icon="record_voice_over"
+            label="New question"
+            primary
+            fixed
+        />
+        </Link>
     </div>
   </div>
 )
