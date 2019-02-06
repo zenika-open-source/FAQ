ae85e8abd37ef52b64e2975ef7ba0d368d1dfc83
Errorname <do.not.press.enter@gmail.com>
2019-02-06 14:46:54 +0100

    :sparkles: Minified share url

--- src/scenes/Question/scenes/Read/components/Share/Share.jsx
+++ src/scenes/Question/scenes/Read/components/Share/Share.jsx
@@ -2,37 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import copy from 'copy-to-clipboard'
 
+import { routing } from 'services'
+
 import Dropdown, { DropdownItem } from 'components/Dropdown'
 
 import Button from 'components/Button'
 
 import './Share.css'
 
-const Share = props => (
-  <div className="share">
-    <Dropdown
-      button={<Button icon="share" link style={{ padding: '0.2rem' }} />}
-    >
-      <DropdownItem
-        icon={<i className="fab fa-facebook" />}
-        onClick={() => {
-          let url =
-            'https://work.facebook.com/sharer.php?display=popup&u=' +
-            window.location.href +
-            '&quote=' +
-            encodeURI(props.node.question.title)
-          let options = 'toolbar=0,status=0,resizable=1,width=626,height=436'
-          window.open(url, 'sharer', options)
-        }}
-      >
-        Workplace
-      </DropdownItem>
-      <DropdownItem icon="link" onClick={() => copy(window.location.href)}>
-        Copy link
-      </DropdownItem>
-    </Dropdown>
-  </div>
-)
+const Share = props => {
+  const shareUrl = routing.getShareUrl(props.node.id)
+  return (
+    <div className="share">
+      <Dropdown button={<Button icon="share" link style={{ padding: '0.2rem' }} />}>
+        <DropdownItem
+          icon={<i className="fab fa-facebook" />}
+          onClick={() => {
+            let url =
+              'https://work.facebook.com/sharer.php?display=popup&u=' +
+              shareUrl +
+              '&quote=' +
+              encodeURI(props.node.question.title)
+            let options = 'toolbar=0,status=0,resizable=1,width=626,height=436'
+            window.open(url, 'sharer', options)
+          }}
+        >
+          Workplace
+        </DropdownItem>
+        <DropdownItem icon="link" onClick={() => copy(shareUrl)}>
+          Copy link
+        </DropdownItem>
+      </Dropdown>
+    </div>
+  )
+}
 
 Share.propTypes = {
   node: PropTypes.object.isRequired
