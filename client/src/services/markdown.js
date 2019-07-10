7b8ad836d49ef6669ab6d83d088adfd6b44acced
Errorname <do.not.press.enter@gmail.com>
2019-07-10 15:49:06 +0200

    :bug: Fix markdown link detection

--- client/src/services/markdown.js
+++ client/src/services/markdown.js
@@ -18,10 +18,14 @@ class Markdown {
     return this.emoji(title)
   }
 
-  html(text) {
+  text(text) {
     text = this.emoji(text)
     text = this.removeEmTagInLink(text)
 
+    return this.showdown.makeHtml(text)
+  }
+
+  html(text) {
     return (
       <div className="mde-preview">
         <div
@@ -32,7 +36,7 @@ class Markdown {
             border: '0'
           }}
           dangerouslySetInnerHTML={{
-            __html: this.showdown.makeHtml(text)
+            __html: this.text(text)
           }}
         />
       </div>
@@ -63,9 +67,17 @@ class Markdown {
     const sanitize = txt => txt.replace(/<em>/g, '').replace(/<\/em>/g, '')
 
     text = text.replace(/(\[.*\]\()?(https?:\/\/\S*)/gim, (link, a, b) => {
-      if (a && b[b.length - 1] === ')') {
+      const endOfB = b.split('').reduce(
+        (acc, c, i) => {
+          if (acc.stack === 0) return acc
+          return { i, stack: acc.stack + (c === '(' ? 1 : c === ')' ? -1 : 0) }
+        },
+        { i: 0, stack: 1 }
+      )
+
+      if (a && endOfB.stack === 0) {
         // Means it's a markdown link
-        return a + sanitize(b)
+        return a + sanitize(b.substr(0, endOfB.i)) + b.substr(endOfB.i)
       }
 
       return `<a href="${sanitize(link)}" target="_blank" rel="noopener noreferrer">${link}</a>`
