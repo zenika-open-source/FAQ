55ef9219f09702f64c6251c7f9a1e39cf4baeb88
Errorname <do.not.press.enter@gmail.com>
2019-08-08 11:24:13 +0200

    :children_crossing: Add contact email in footer

--- client/src/scenes/App/components/Footer/Footer.jsx
+++ client/src/scenes/App/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import './Footer.scss'
+
 const Footer = () => (
   <div className="footer">
     <a href="https://www.algolia.com" target="_blank" rel="noopener noreferrer">
       <img src="/img/brands/powered-by-algolia.svg" alt="Powered by Algolia" />
     </a>
+    <span className="contact">
+      <i className="material-icons">mail</i>
+      <a href="mailto:contact@faq.team">contact@faq.team</a>
+    </span>
   </div>
 )
 
