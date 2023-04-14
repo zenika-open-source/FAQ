8583bc9e5da3b5113a22c5b5eeeb4aa27c079183
Thibaud <thibaud.brault@protonmail.com>
2023-04-14 17:18:24 +0200

    :green_heart: modify method used for tracing test

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   reporter: process.env.CI && [['junit', { outputFile: 'results.xml' }]],
   use: {
+    trace: process.env.CI && 'on',
     headless: true,
     locale: 'fr-FR'
   }
