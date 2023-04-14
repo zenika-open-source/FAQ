9f61525e00292790324996f6f05861dc1dd101cf
Thibaud <thibaud.brault@protonmail.com>
2023-04-14 12:14:44 +0200

    :green_heart: add xvfb

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   reporter: process.env.CI && [['junit', { outputFile: 'results.xml' }]],
   use: {
+    headless: true,
     locale: 'fr-FR'
   }
 })
