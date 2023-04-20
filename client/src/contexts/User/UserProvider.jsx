b0737887a1e06c9892a17c79e5d20c2aa173bf38
Thibaud <thibaud.brault@protonmail.com>
2023-04-20 16:21:21 +0200

    :sparkles: possibility to access profile for users not connected

--- client/src/contexts/User/UserProvider.jsx
+++ client/src/contexts/User/UserProvider.jsx
@@ -8,11 +8,25 @@ import { GET_ME } from './queries'
 export const UserContext = React.createContext({})
 
 const UserProvider = ({ children }) => {
+  let value
+
   const { isAuth } = useAuth()
 
   const { data } = useQuery(GET_ME, { skip: !isAuth })
 
-  const value = useMemo(() => isAuth && data && data.me, [isAuth, data])
+  value = useMemo(() => isAuth && data && data.me, [isAuth, data])
+
+  if (process.env.REACT_APP_DISABLE_AUTH === 'true') {
+    value = {
+      id: 'clgosxxdj00080894xqetwi5s',
+      admin: false,
+      name: 'playwrightTest',
+      email: 'faq-user-no-auth@zenika.com',
+      picture:
+        'https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg',
+      __typename: 'User'
+    }
+  }
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
