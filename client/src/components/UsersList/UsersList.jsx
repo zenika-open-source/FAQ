3a0a2bed39cab20a417fcd16dbdd7b4d6ea20a74
Thibaud Brault <thibaud.brault@zenika.com>
2023-04-28 17:06:52 +0200

    :construction: create filter dropdown + list of users

--- client/src/components/UsersList/UsersList.jsx
+++ client/src/components/UsersList/UsersList.jsx
@@ -1,39 +1,86 @@
 import { useQuery } from '@apollo/react-hooks'
-import { List, Loading } from 'components'
-import { useEffect, useState } from 'react'
+import { Dropdown, Loading } from 'components'
+import { useState } from 'react'
 import { GET_TAG_CATEGORIES, GET_USERS } from './queries'
-import { ListItem } from 'components/List'
+import { DropdownItem } from 'components/Dropdown'
+import { getIntl } from 'services'
+import uniqBy from 'lodash/uniqBy'
+import pull from 'lodash/pull'
 
 import './UsersList.css'
 
 const UsersList = () => {
-  const [users, setUsers] = useState(null)
+  const intl = getIntl(UsersList)
 
-  //   const { data } = useQuery(GET_TAG_CATEGORIES)
-  //   console.log(data.configuration.tagCategories.find(category => category.name === 'services'))
-  const { data, loading } = useQuery(GET_USERS, {
+  const [users, setUsers] = useState(null)
+  const [services, setServices] = useState(null)
+  const [filters, setFilters] = useState([])
+
+  useQuery(GET_TAG_CATEGORIES, {
+    onCompleted: data =>
+      setServices(
+        data.configuration.tagCategories.find(category => category.name === 'services').labels
+      )
+  })
+
+  const { loading } = useQuery(GET_USERS, {
     onCompleted: data => setUsers(data.users)
   })
 
-  console.log(users)
+  const addFilter = filter => {
+    setFilters(uniqBy([...filters, filter]))
+  }
+
+  const removeFilter = filter => {
+    setFilters(pull([...filters], filter))
+  }
 
   if (loading) return <Loading />
 
   return (
-    <ul className="usersList">
-      {users?.map(user => (
-        <li key={user.id} className="userElement">
-          <div style={{ marginRight: '1rem' }}>
-            <i className="material-icons">{user.admin ? 'admin_panel_settings' : 'person'}</i>
-          </div>
-          <div className="userInfo">
-            <p className="userName">{user.name}</p>
-            <p className="userEmail">{user.email}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <section>
+      <Dropdown className="dropServices" button={intl('dropdown')}>
+        {services?.map(service => (
+          <DropdownItem onClick={() => addFilter(service.name)} key={service.id}>
+            {service.name}
+          </DropdownItem>
+        ))}
+      </Dropdown>
+      <ul className="filtersList">
+        {filters.map(filter => (
+          <li>
+            <i className="material-icons">label</i>
+            <span>{filter}</span>
+            <i onClick={() => removeFilter(filter)} className="material-icons">
+              close
+            </i>
+          </li>
+        ))}
+      </ul>
+      <ul className="usersList">
+        {users?.map(user => (
+          <li key={user.id} className="userElement">
+            <div className="userIcon">
+              <i className="material-icons">{user.admin ? 'admin_panel_settings' : 'person'}</i>
+            </div>
+            <div className="userInfo">
+              <span className="userName">{user.name}</span>
+              <span className="userEmail">{user.email}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
   )
+}
+
+UsersList.translations = {
+  en: {
+    dropdown: 'Filter by specialities'
+  },
+  fr: {
+    dropdown: 'Filtrer par spécialités'
+  }
 }
 
 export default UsersList
