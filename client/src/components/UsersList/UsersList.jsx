5758351389366917a63d99c1aedb0590cc7dd961
Thibaud <thibaud.brault@protonmail.com>
2023-05-12 11:54:57 +0200

    :art: reuse component for search results

--- client/src/components/UsersList/UsersList.jsx
+++ client/src/components/UsersList/UsersList.jsx
@@ -1,12 +1,11 @@
 import { useQuery } from '@apollo/react-hooks'
 import { Loading } from 'components'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getIntl } from 'services'
 import { GET_TAG_CATEGORIES, GET_USERS } from './queries'
 
 import './UsersList.css'
 import Specialist from './components/Specialist'
-import SpecialtiesList from './components/SpecialtiesList'
 
 const UsersList = () => {
   const intl = getIntl(UsersList)
@@ -14,7 +13,7 @@ const UsersList = () => {
   const [users, setUsers] = useState(null)
   const [specialists, setSpecialists] = useState(null)
   const [services, setServices] = useState(null)
-  const [results, setResults] = useState([])
+  const [filteredUsers, setFilteredUsers] = useState([])
 
   useQuery(GET_TAG_CATEGORIES, {
     onCompleted: data =>
@@ -33,17 +32,20 @@ const UsersList = () => {
   const searchUsers = text => {
     let matches = []
     if (text.length > 0) {
-      matches =
-        results &&
-        users?.filter(user => {
-          const regex = new RegExp(`^${text}`, `gi`)
-          return user.name.match(regex)
-        })
-      setResults(matches?.slice(0, 5))
+      matches = users?.filter(user => {
+        const regex = new RegExp(`^${text}`, `gi`)
+        return user.name.match(regex)
+      })
+      setFilteredUsers(matches?.slice(0, 5))
     } else {
-      setResults([])
+      setFilteredUsers([])
     }
   }
+
+  useEffect(() => {
+    setSpecialists(specialists)
+  }, [specialists])
+
   if (loading) return <Loading />
 
   return (
@@ -56,25 +58,11 @@ const UsersList = () => {
         placeholder={intl('search')}
         onChange={e => searchUsers(e.target.value)}
       />
-      {results.length > 0 && (
+      {filteredUsers.length > 0 && (
         <div className="resultsContainer">
-          <ul className="resultsList">
-            {results.map(result => (
-              <li className="resultsEl" key={result.id}>
-                <p className="resultName">{result.name}</p>
-                <div className="userRight">
-                  <div className="userSpecialties">
-                    {result?.specialties.length > 0 &&
-                      result.specialties.map(specialty => (
-                        <div key={specialty.name} className="userSpecialty">
-                          <span>{specialty.name}</span>
-                          <i className="material-icons">close</i>
-                        </div>
-                      ))}
-                  </div>
-                  <SpecialtiesList services={services} />
-                </div>
-              </li>
+          <ul className="usersList">
+            {filteredUsers.map(user => (
+              <Specialist services={services} specialist={user} key={user.id} />
             ))}
           </ul>
         </div>
