8267d2045edc83d08d3849aa4ce3e2fa7d50fad8
Thibaud <thibaud.brault@protonmail.com>
2023-05-16 15:59:45 +0200

    :art: add default value for graphql and rest endpoints in front

--- client/src/services/apollo.js
+++ client/src/services/apollo.js
@@ -40,7 +40,7 @@ const apollo = new ApolloClient({
       }
     }),
     new HttpLink({
-      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
+      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || '/gql'
     })
   ]),
   cache: apolloCache,
