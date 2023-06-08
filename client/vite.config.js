cf07c6e7205843d627b14fa201493a4b08bc220b
Thibaud Brault <thibaud.brault@zenika.com>
2023-06-08 17:15:52 +0200

    :recycle: remove ci-info + upgrade @vitejs/plugin-react

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dns from 'dns'
-import { isCI } from 'ci-info'
 
 dns.setDefaultResultOrder('verbatim')
 
@@ -10,19 +9,19 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@helpers': path.resolve(__dirname, 'src/helpers'),
-      '@scenes': path.resolve(__dirname, 'src/scenes'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@styles': path.resolve(__dirname, 'src/styles')
+      components: path.resolve(__dirname, 'src/components'),
+      contexts: path.resolve(__dirname, 'src/contexts'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      scenes: path.resolve(__dirname, 'src/scenes'),
+      services: path.resolve(__dirname, 'src/services'),
+      styles: path.resolve(__dirname, 'src/styles')
     }
   },
   build: {
     outDir: 'build'
   },
   server: {
-    open: !isCI,
+    open: process.env.CI !== 'true',
     port: 3000
   }
 })
