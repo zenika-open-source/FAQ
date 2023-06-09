52e82db2cbbb217f7b93246252d952588429134d
Thibaud Brault <thibaud.brault@zenika.com>
2023-06-09 11:33:52 +0200

    :wrench: remove server url env variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dns from 'dns'
 
 dns.setDefaultResultOrder('verbatim')
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
+  const serverUrl = 'http://localhost:4000'
   return {
     plugins: [react()],
     resolve: {
@@ -27,12 +27,12 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       proxy: {
         '/rest': {
-          target: env.VITE_SERVER_URL,
+          target: serverUrl,
           changeOrigin: true,
           secure: false
         },
         '/gql': {
-          target: env.VITE_SERVER_URL,
+          target: serverUrl,
           changeOrigin: true,
           secure: false
         }
