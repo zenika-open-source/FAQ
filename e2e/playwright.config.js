f9d3d503e7ed5988c0098f0b0605ca88901c08b1
Thibaud <thibaud.brault@protonmail.com>
2023-06-29 16:45:59 +0200

    :green_heart: add parallelism to tests in ci

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     trace: process.env.CI && 'on',
     screenshot: process.env.CI && 'on',
     video: process.env.CI && 'on',
+    workers: process.env.CI ? 2 : undefined,
     headless: true,
     locale: 'fr-FR',
     baseURL: 'http://localhost:3000'
