9f9f5c23e536c2949dfcad04c1896a6de1bca8f5
Thibaud <thibaud.brault@protonmail.com>
2023-06-29 16:50:07 +0200

    :wrench: update playwright config file

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   reporter: process.env.CI && [['junit', { outputFile: 'results.xml' }]],
+  workers: process.env.CI ? 2 : undefined,
   use: {
     trace: process.env.CI && 'on',
     screenshot: process.env.CI && 'on',
     video: process.env.CI && 'on',
-    workers: process.env.CI ? 2 : undefined,
     headless: true,
     locale: 'fr-FR',
     baseURL: 'http://localhost:3000'
