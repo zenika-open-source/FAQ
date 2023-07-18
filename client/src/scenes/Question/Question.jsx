19743800211c8a6ad869e7bb16e3379044c79dbb
Thibaud Brault <thibaud.brault@zenika.com>
2023-07-18 11:35:45 +0200

    :recycle: clean up question routes

--- client/src/scenes/Question/Question.jsx
+++ client/src/scenes/Question/Question.jsx
@@ -1,25 +1,17 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { withNode } from './Question.container'
+import QuestionContainer from './Question.container'
 
-import Answer from './scenes/Answer'
 import Edit from './scenes/Edit'
 import Random from './scenes/Random'
-import Read from './scenes/Read'
-
-const withNodeRead = withNode(Read)
-const withNodeEdit = withNode(Edit)
-const withNodeAnswer = withNode(Answer)
 
 const Question = () => {
   return (
     <Routes>
-      <Route path="/new" element={<Edit />} />
-      <Route path="/random/:tag?" element={<Random />} />
-      <Route path="/:slug" element={withNodeRead} />
-      <Route path="/:slug/edit" element={withNodeEdit} />
-      <Route path="/:slug/answer" element={withNodeAnswer} />
-      <Route render={() => <Navigate to="/" />} />
+      <Route path="new" element={<Edit />} />
+      <Route path="random/:tag?" element={<Random />} />
+      <Route path=":slug/*" element={<QuestionContainer />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
