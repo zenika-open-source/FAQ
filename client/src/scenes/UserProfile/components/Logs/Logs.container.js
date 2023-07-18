f066b2a7e1780a4a7bd9771db8585634895642c4
Thibaud Brault <thibaud.brault@zenika.com>
2023-07-18 17:21:39 +0200

    :recycle: useLocation instead of useNavigate

--- client/src/scenes/UserProfile/components/Logs/Logs.container.js
+++ client/src/scenes/UserProfile/components/Logs/Logs.container.js
@@ -1,19 +1,18 @@
 import { withError, withPagination } from 'components'
 import { compose, unserialize } from 'helpers'
+import { useLocation } from 'react-router'
 import { query } from 'services/apollo'
 
-import { meHistory } from './queries'
-
-import { useNavigate } from 'react-router'
 import Logs from './Logs'
+import { meHistory } from './queries'
 
 const ENTRIES_PER_PAGE = 15
 
 export default compose(
   query(meHistory, {
     variables: props => {
-      const navigate = useNavigate()
-      const { page } = unserialize(props.location.search)
+      const location = useLocation()
+      const { page } = unserialize(location.search)
 
       return {
         id: props.userId,
