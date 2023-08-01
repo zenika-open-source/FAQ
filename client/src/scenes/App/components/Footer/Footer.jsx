687cd6626dae777f374cd386f9de801baed798ef
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-01 17:39:09 +0200

    :recycle: add tailwind for footer

--- client/src/scenes/App/components/Footer/Footer.jsx
+++ client/src/scenes/App/components/Footer/Footer.jsx
@@ -1,12 +1,10 @@
-import './Footer.scss'
-
 const Footer = () => (
-  <footer className="footer">
+  <footer className="h-[30px] flex items-center justify-evenly">
     <a href="https://www.algolia.com" target="_blank" rel="noopener noreferrer">
       <img src="/img/brands/powered-by-algolia.svg" alt="Powered by Algolia" />
     </a>
-    <span className="contact">
-      <i className="material-icons">mail</i>
+    <span className="flex items-center">
+      <i className="material-icons text-[20px] pt-px mr-[4px]">mail</i>
       <a href={`mailto:contact@${import.meta.env.VITE_FAQ_URL}`}>
         contact@{import.meta.env.VITE_FAQ_URL}
       </a>
