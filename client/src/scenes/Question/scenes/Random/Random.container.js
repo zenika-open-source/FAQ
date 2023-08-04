d0bd64341ba9d3c46bc304f5f410fd989d2344e8
Thibaud Brault <77327446+thibaudbrault@users.noreply.github.com>
2023-08-04 10:13:51 +0200

    :ambulance: fix crash when trying to see the history of a question (#392)
    This pr fix a crash when trying to access the history of a question because the container was still using react-router v4 props and not react-router v6 hooks

--- client/src/scenes/Question/scenes/Random/Random.container.js
+++ client/src/scenes/Question/scenes/Random/Random.container.js
@@ -6,12 +6,16 @@ import { query } from 'services/apollo'
 import { GET_RANDOM } from './queries'
 
 import Random from './Random'
+import { useParams } from 'react-router'
 
 export default compose(
   query(GET_RANDOM, {
-    variables: (props) => ({
-      tag: props.match.params.tag,
-    }),
+    variables: () => {
+      const params = useParams()
+      return {
+        tag: params.tag,
+      }
+    },
     fetchPolicy: 'network-only',
   }),
   withLoading(getIntl(Random)('loading')),
